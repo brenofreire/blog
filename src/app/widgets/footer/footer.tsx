a664b4e16d4a0646b09c4713b928d6988e88a92d
Breno Freire <breno@arionkkoder.com>
2024-09-22 02:01:28 -0300

    feat: updates favicon]

--- src/app/widgets/footer/footer.tsx
+++ src/app/widgets/footer/footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer>
       <div className="flex mx-auto my-6 w-fit">
-        <small>v0.1.0</small>
+        <small>v0.1.1</small>
       </div>
     </footer>
   );
